cdce64a81c84fcc382be04afa8948d885f25328a
Robert Zacharias <bobby.zacharias@gmail.com>
2017-04-09 21:17:57 -0400

    git cry

--- gpio-digital-read.py
+++ gpio-digital-read.py
@@ -2,43 +2,34 @@
 Simple GPIO digital read for RPi
 
 Does digital read of physical pin 22 (GPIO pin 6)
+and writes value to physical pin 16 (GPIO pin 4)
 
-Robert Zacharias 2-27-17
+not currently reading keyboard input; getch doesn't seem to be working.
+
+Robert Zacharias 4-10-17
 
 """
 
 
 import RPi.GPIO as G
 import time
+import getch
 
 G.setmode(G.BOARD)
 
+G.setup(16, G.OUT)
 G.setup(22, G.IN)
 
 while(True):
         if (G.input(22)):
+                G.output(16, True)
                 print("digital read 22 HIGH")
         else:
+                G.output(16, False)
                 print("digital read 22 LOW")
-        time.sleep(1)
-
-
-"""
-while(True):
-	key = ord(getch())
-	if key != 0:
-		G.output(16, True)
-		G.output(18, False)
-	else:
-		G.output(18, True)
-		G.output(16, False)
-
-
-while(True):
-	G.output(16, True)
-	G.output(18, False)
-	time.sleep(1)
-	G.output(18, True)
-	G.output(16, False)
-	time.sleep(1)
-"""
+        char = getch.getch()
+        if char == "q":
+                print("quitting and resetting pins")
+                G.cleanup()
+                exit()
+        time.sleep(0.1)
